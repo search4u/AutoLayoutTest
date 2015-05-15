69c19cd8b90ee4a16f2182a4bba080823dee26a9
hellen zhou <50406166@qq.com>
2015-05-15 11:48:46 +0800

    first commit at bottle

---
+++ AutoLayoutTest/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  AutoLayoutTest
+//
+//  Created by bottle on 15-5-14.
+//  Copyright (c) 2015年 bottle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+- (IBAction)btnTap:(UIButton *)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLayoutConstraint
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnTap:(UIButton *)sender {
+    if ([sender.currentTitle isEqualToString:@"X"]) {
+        [sender setTitle:@"A long title" forState:UIControlStateNormal];
+    }else {
+         [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
+}
+@end
